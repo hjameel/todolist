d24ef34e1545dcc99a004f5b495d11192c20da0b
Hanif Jameel <hanif.jameel@btinternet.com>
2015-05-31 15:08:57 +0100

    Tidy and fix tests

--- test_todo.py
+++ test_todo.py
@@ -1,9 +1,8 @@
-from flask import Flask
 from flask.ext.testing import LiveServerTestCase
 from selenium import webdriver
 
 import unittest
-import flashcards
+import todo
 
 class EndToEndTest(LiveServerTestCase):
     def setUp(self):
@@ -13,7 +12,7 @@ class EndToEndTest(LiveServerTestCase):
         self.driver.close()
 
     def create_app(self):
-        app = flashcards.app
+        app = todo.app
         app.config["Testing"] = True
         app.config["LIVESERVER_PORT"] = 8943
         return app
@@ -28,15 +27,13 @@ class EndToEndTest(LiveServerTestCase):
     def test_adds_and_displays_a_card(self):
         self.driver.get(self.get_server_url())
 
-        front_text = "The front"
-        back_text = "The back"
+        todo_text = "This is a todo"
 
-        self.driver.find_element_by_id("id_front").send_keys(front_text)
-        self.driver.find_element_by_id("id_back").send_keys(back_text)
+        self.driver.find_element_by_id("id_todo").send_keys(todo_text)
         self.driver.find_element_by_id("id_add").click()
 
         self.driver.get(self.get_server_url())
 
         self.assertIn(
-                front_text,
+                todo_text,
                 self.driver.find_element_by_tag_name("body").text)
--- todo.py
+++ todo.py
@@ -1,4 +1,3 @@
-import config
 import os
 
 from contextlib import closing
